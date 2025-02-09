c847302f22b8b8f24e4b2c8a93f6564f1d2d24a0
jayfromkorea <jayprgacc@gmail.com>
2025-02-09 22:33:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dp[1001], n;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    cin >> n;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n] << "\n";
+    return 0;
+}
