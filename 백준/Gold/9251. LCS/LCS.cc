a6bff17585886ef1948cf9d499d1e579c9c2401f
jayfromkorea <jayprgacc@gmail.com>
2025-03-23 22:47:49 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001];
+
+int main() {
+	string s1, s2;
+	
+	cin >> s1 >> s2;
+	
+	for (int i = 0; i < s1.length(); i++){
+		for (int j = 0; j < s2.length(); j++){
+			if(s1[i] == s2[j]){
+				dp[i+1][j+1] = dp[i][j] + 1;
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+			}
+		}
+	}
+	cout << dp[s1.length()][s2.length()] << "\n";
+}
