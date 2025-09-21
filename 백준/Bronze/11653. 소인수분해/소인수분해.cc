1f9c169229957f76c469ba0da08842c5c7e9e9f9
jayfromkorea <jayprgacc@gmail.com>
2025-09-21 22:37:06 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	
+	cin >> n;
+	if (n == 1) {
+		return 0;
+	}
+	for (int i = 2; i <= n; i++){
+		while (n % i == 0) {
+			cout << i << '\n';
+			n /= i;
+		}
+	}
+}
