23cbc81d1a7d00bbd68b24a95a20dadd5ab4a2df
jayfromkorea <jayprgacc@gmail.com>
2025-09-28 23:23:09 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+	int a, b;
+	
+	cin >> a >> b;
+	
+	cout << a / b << ".";
+	
+	int cur = a % b;
+	
+	for (int i = 0; i < 9; i++){
+		if (cur == 0) return 0;
+		cur *= 10;
+		cout << cur / b;
+		cur %= b;
+	}
+	return 0;
+}
