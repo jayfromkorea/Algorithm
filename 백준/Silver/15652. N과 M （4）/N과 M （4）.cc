d993da72e706a18d63151c24d636677a94c5c4b2
jayfromkorea <jayprgacc@gmail.com>
2025-10-19 21:57:09 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//bool used[10];
+int arr[10];
+int n, m;
+
+void func(int k, int last){
+	if (k == m) {
+		for (int i = 0; i < m; i++){
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	
+	
+	for (int i = last; i <= n; i++){
+		//if (used[i]) continue;
+		arr[k] = i;
+		//used[i] = 1;
+		func(k + 1, i);
+		//used[i] = 0;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    
+    func(0, 1);
+}
