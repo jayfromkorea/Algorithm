80f26cb9b3763eee209305853871debb56fb91cb
jayfromkorea <jayprgacc@gmail.com>
2025-11-02 12:29:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 260 ms, Memory: 6380 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+bool vis[1001];
+
+int main() {
+    int v, e;
+    cin >> v >> e;
+
+    vector<vector<int>> adj(v + 1); // allocate adjacency list
+    queue<int> q;
+    int str, end;
+
+    // input edges
+    for (int i = 0; i < e; i++) {
+        cin >> str >> end;
+        adj[str].push_back(end);
+        adj[end].push_back(str);
+    }
+
+    // BFS
+    int count = 0;
+    for (int i = 1; i <= v; i++) {
+        if (vis[i]) continue;
+        count++;
+        q.push(i);
+        vis[i] = true;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for (int nxt : adj[cur]) {
+                if (vis[nxt]) continue;
+                q.push(nxt);
+                vis[nxt] = true;
+            }
+        }
+    }
+
+    cout << count << '\n';
+    return 0;
+}
