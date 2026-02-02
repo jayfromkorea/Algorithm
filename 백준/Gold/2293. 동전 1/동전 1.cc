abf03884165877b2516f889446eddb203f97a211
jayfromkorea <jayprgacc@gmail.com>
2026-02-02 23:51:09 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[10001];
+int coin[105];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    
+    for (int i = 0; i < n; i++){
+        cin >> coin[i];
+    }
+    
+    dp[0] = 1;
+    
+    for (int i = 0; i < n; i++){
+        for (int j = coin[i]; j <= k; j++){
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+    cout << dp[k] << '\n';
+}
